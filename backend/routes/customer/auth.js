265c1fbfa5cda0388deb50009cc38072e858bd2f
randima217 <152683475+randima-edussuriya@users.noreply.github.com>
2025-07-04 09:20:11 +0530

    renamed admin customer routes for clear understand

--- backend/routes/customer/auth.js
+++ backend/routes/customer/auth.js
@@ -3,7 +3,7 @@ import { login, logout, signup, test } from '../../controllers/customer/auth.js'
 
 const router = express.Router();
 
-router.post('/signup', signup)
+router.post('/signup', signup);
 router.post('/login', login);
 router.post('logout', logout);
 router.get('/test', test);
