4d1b0d168d56d3fb194d3159970b1149bbfb705f
randima217 <152683475+randima-edussuriya@users.noreply.github.com>
2025-07-06 23:32:12 +0530

    Sidebar menu active

--- frontend-admin/src/components/Sidebar/Sidebar.jsx
+++ frontend-admin/src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../../assets/icons/logo.png'
 import { Link } from 'react-router-dom'
 import './Sidebar.css'
+import { NavLink } from 'react-router-dom'
 
 function Sidebar() {
     return (
@@ -19,58 +20,123 @@ function Sidebar() {
                 <hr className='text-dark' />
 
                 <div className='list-group list-group-flush rounded'>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    <NavLink
+                        to={'/staff-management'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-person-gear fs-5 me-3"></i>
                         <span>Staff Management</span>
-                    </Link>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    </NavLink>
+                    <NavLink
+                        to={'/customer-management'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-people fs-5 me-3"></i>
                         <span>Customer Management</span>
-                    </Link>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    </NavLink>
+                    <NavLink
+                        to={'/home'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-bookmarks fs-5 me-3"></i>
                         <span>Category Management</span>
-                    </Link>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    </NavLink>
+                    <NavLink
+                        to={'/home'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-diagram-3 fs-5 me-3"></i>
                         <span>Item Management</span>
-                    </Link>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    </NavLink>
+                    <NavLink
+                        to={'/home'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-border-all fs-5 me-3"></i>
                         <span>Order Management</span>
-                    </Link>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    </NavLink>
+                    <NavLink
+                        to={'/home'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-truck fs-5 me-3"></i>
                         <span>Delivery Management</span>
-                    </Link>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    </NavLink>
+                    <NavLink
+                        to={'/home'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-sliders2-vertical fs-5 me-3"></i>
                         <span>Repair Management</span>
-                    </Link>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    </NavLink>
+                    <NavLink
+                        to={'/home'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-arrow-clockwise fs-5 me-3"></i>
                         <span>Reorder Management</span>
-                    </Link>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    </NavLink>
+                    <NavLink
+                        to={'/home'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-calendar-day fs-5 me-3"></i>
                         <span>Day off Management</span>
-                    </Link>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    </NavLink>
+                    <NavLink
+                        to={'/home'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-cursor fs-5 me-3"></i>
                         <span>Loyalty Program Management</span>
-                    </Link>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    </NavLink>
+                    <NavLink
+                        to={'/home'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-star fs-5 me-3"></i>
                         <span>Feedback and rating Management</span>
-                    </Link>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    </NavLink>
+                    <NavLink
+                        to={'/home'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-telephone-forward fs-5 me-3"></i>
                         <span>Customer Support Management</span>
-                    </Link>
-                    <Link to={'/home'} className='list-group-item py-2'>
+                    </NavLink>
+                    <NavLink
+                        to={'/home'}
+                        className={({ isActive }) =>
+                            'list-group-item py-2' + (isActive ? ' active_link' : '')
+                        }
+                    >
                         <i className="bi bi-flag fs-5 me-3"></i>
                         <span>Reports</span>
-                    </Link>
+                    </NavLink>
                 </div>
             </div>
         </>
