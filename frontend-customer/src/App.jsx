e2a904db4e376a87b15d3a8ba0b80e438e2df8f9
randima217 <152683475+randima-edussuriya@users.noreply.github.com>
2025-07-08 18:06:45 +0530

    removed unwanted file

--- frontend-customer/src/App.jsx
+++ frontend-customer/src/App.jsx
@@ -3,55 +3,53 @@ import './App.css'
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
-import Home from './pages/Home/'
 import ComingSoon from './pages/ComingSoon'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Cart from './pages/Cart'
-import Test from './pages/Test'
+import { Container } from 'react-bootstrap'
+import Home from './pages/Home'
 
 const Layout = () => {
   return (
-    <>
-      <Header />
-      <Outlet />
+    <div className='d-flex flex-column min-vh-100'>
+      <div className='flex-grow-1'>
+        <Header />
+        <Outlet />
+      </div>
       <Footer />
-    </>
+    </div>
   )
 }
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: '',
       element: <Layout />,
       errorElement: <div>Not Found: Invalid URL</div>,
       children: [
         {
-          path: '/',
+          path: '',
           element: <Home />,
         },
         {
-          path: '/login',
+          path: 'login',
           element: <Login />
         },
         {
-          path: '/signup',
+          path: 'signup',
           element: <Signup />
         },
         {
-          path: '/cart',
+          path: 'cart',
           element: <Cart />
         },
         {
-          path: '/coming_soon',
+          path: 'coming_soon',
           element: <ComingSoon />
         }
       ]
-    },
-    {
-      path: "/test",
-      element: <Test />
     }
   ])
 
