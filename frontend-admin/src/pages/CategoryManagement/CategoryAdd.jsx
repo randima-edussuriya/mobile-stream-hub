dde15bb0e6b60d2dee6ac9f7c55999b5ab0f235d
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-07-10 14:55:03 +0530

    created category add UI

---
+++ frontend-admin/src/pages/CategoryManagement/CategoryAdd.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Form, Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import categoryValidation from '../../validations/categoryValidation'
+
+function CategoryAdd() {
+    const [formData, setFormData] = useState({
+        categoryName: '',
+        categoryType: '',
+    })
+    const [errors, setErrors] = useState({});
+
+    const navigate = useNavigate();
+
+    /* -----------------------------------------------------------------
+          Handle form input changes 
+    --------------------------------------------------------------------*/
+    const handleChange = (e) => {
+        setFormData(prev => ({ ...prev, [e.target.name]: e.target.value }));
+    }
+
+    /* -----------------------------------------------------------------
+           Handle form submit
+     --------------------------------------------------------------------*/
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const validationErrors = categoryValidation(formData);
+        setErrors(validationErrors);
+        if (Object.keys(validationErrors).length === 0) {
+            try {
+                const res = await axios.post('http://localhost:5000/api/category', formData)
+                if (res.data.success) {
+                    toast.success(res.data.message, { position: "top-center" })
+                    navigate('/category-management');
+                } else {
+                    toast.error(res.data.message, { position: "top-center" });
+                }
+            } catch (err) {
+                console.error(err);
+                toast.error('An error occurred. Please try again.', { position: "top-center" });
+            }
+        }
+    }
+    return (
+        <Container fluid className='d-flex justify-content-center align-items-center  mt-3 mb-5 py-0 rounded'>
+            <Container className='col-10 col-sm-6 p-3  rounded bg-secondary-subtle shadow_white'>
+                <h3 className='text-center mb-3'>Add Category</h3>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group className="mb-3" controlId="formGroupName">
+                        <Form.Label>Category Name</Form.Label>
+                        <Form.Control type="text" placeholder="Enter category name" name='categoryName' onChange={handleChange} />
+                        {errors.categoryName && <span className='text-danger'>{errors.categoryName}</span>}
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formGroupCategoryType">
+                        <Form.Label>Category Type</Form.Label>
+                        <Form.Select value={formData.categoryType} name='categoryType' onChange={handleChange}>
+                            <option value=''>Select category type</option>
+                            <option value='accessory'>Accessory</option>
+                            <option value='phone'>Phone</option>
+                            <option value='repair part'>Repair part</option>
+                        </Form.Select>
+                        {errors.categoryType && <span className='text-danger'>{errors.categoryType}</span>}
+                    </Form.Group>
+
+                    <div className='mb-3'>
+                        <Button className='btn_main_dark me-3 shadow' type='submit'>Add</Button>
+                        <Button variant='outline-danger' className='btn_style me-3 border-2 shadow' type='reset'>Reset</Button>
+                    </div>
+                </Form>
+            </Container>
+        </Container>
+    )
+}
+
+export default CategoryAdd
