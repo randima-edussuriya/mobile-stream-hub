65d811393bc889d182488a50d6cf34052f5fa744
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-07-10 14:54:18 +0530

    completed addCategory API

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -2,16 +2,16 @@ import db from "../db.js";
 
 export const addCategory = async (req, res) => {
     try {
-        const name = req.body.name?.trim();
-        const type = req.body.type?.trim();
+        const categoryName = req.body.categoryName?.trim();
+        const categoryType = req.body.categoryType?.trim();
 
-        if (!name || !type)
+        if (!categoryName || !categoryType)
             return res.json({ success: false, message: 'Fileds are required' });
 
         //insert category
         const sqlInsert = 'INSERT INTO category (`category_name`, `category_type`) VALUES (?,?)';
-        const values = [name, type]
-        // await db.query(sqlInsert, values);
+        const values = [categoryName, categoryType]
+        await db.query(sqlInsert, values);
         return res.json({ success: true, message: 'Category added successfully' });
     } catch (err) {
         console.error(err);
