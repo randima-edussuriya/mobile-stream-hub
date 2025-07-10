7e267e266818f96d3ef9eab449440ee407ce376a
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-07-10 12:26:14 +0530

    Created category api routes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import customerAuthRoutes from './routes/auth/customerAuth.js'
 import customerRoutes from './routes/customer.js'
 import staffTypeRoutes from './routes/staffType.js'
 import staffUserRoutes from './routes/staffUser.js'
+import categoryRoutes from './routes/category.js'
 
 const app = express();
 app.use(cors({
@@ -24,6 +25,7 @@ app.use('/api/auth/customer', customerAuthRoutes);
 app.use('/api/customer', customerRoutes);
 app.use('/api/staff_type', staffTypeRoutes);
 app.use('/api/staff_user', staffUserRoutes);
+app.use('/api/category', categoryRoutes);
 
 //for test
 import bcrypt, { hash } from 'bcryptjs';
