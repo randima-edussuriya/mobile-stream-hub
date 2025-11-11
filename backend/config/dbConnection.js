8be180866811aee524bf3d0740b45f046b74cbac
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-11-11 19:55:31 +0530

    feat: create db connection pool

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -1,5 +1,18 @@
-const db = () => {
-  //db
-};
+import mysql from "mysql2/promise";
+import "dotenv/config";
 
-export default db;
+const dbPool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10, //adjust based on server capacity
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
+export default dbPool;
