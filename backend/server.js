a4e16925ef993cb7e738a5ee330ffd504b0cda3e
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-11-11 06:32:05 +0530

    refactor: improve .env usage

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,40 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv'
-import adminAuthRoutes from './routes/auth/adminAuth.js'
-import customerAuthRoutes from './routes/auth/customerAuth.js'
-import customerRoutes from './routes/customer.js'
-import staffTypeRoutes from './routes/staffType.js'
-import staffUserRoutes from './routes/staffUser.js'
-import categoryRoutes from './routes/category.js'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import adminAuthRoutes from "./routes/auth/adminAuth.js";
+import customerAuthRoutes from "./routes/auth/customerAuth.js";
+import customerRoutes from "./routes/customer.js";
+import staffTypeRoutes from "./routes/staffType.js";
+import staffUserRoutes from "./routes/staffUser.js";
+import categoryRoutes from "./routes/category.js";
 
 const app = express();
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-    credentials: true
-}));
+const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: [process.env.CLIENT_ADMIN_URL, process.env.CLIENT_CUSTOMER_URL],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
-dotenv.config();
 
 //auth routes
-app.use('/api/auth/admin', adminAuthRoutes);
-app.use('/api/auth/customer', customerAuthRoutes);
+app.use("/api/auth/admin", adminAuthRoutes);
+app.use("/api/auth/customer", customerAuthRoutes);
 
-app.use('/api/customer', customerRoutes);
-app.use('/api/staff_type', staffTypeRoutes);
-app.use('/api/staff_user', staffUserRoutes);
-app.use('/api/category', categoryRoutes);
+app.use("/api/customer", customerRoutes);
+app.use("/api/staff_type", staffTypeRoutes);
+app.use("/api/staff_user", staffUserRoutes);
+app.use("/api/category", categoryRoutes);
 
 //for test
-app.get('/test', async (req, res) => {
-    res.json('Testin happening')
-    const secure = process.env.NODE_ENV === 'development';
-    console.log(secure);
-})
+app.get("/", async (req, res) => {
+  res.json("API is working");
+});
 
-
-const server = app.listen(process.env.PORT, process.env.SERVER_HOST, () => {
-    console.log('Server is listning to port ' + server.address().port);
-})
+const server = app.listen(port, () => {
+  console.log("Server started on port: " + server.address().port);
+});
