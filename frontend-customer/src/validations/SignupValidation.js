47840944f170acc69e04f02fa16b6532d97bf66f
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-11-26 14:46:42 +0530

    feat: change password strength validation logic

--- frontend-customer/src/validations/SignupValidation.js
+++ frontend-customer/src/validations/SignupValidation.js
@@ -1,58 +1,59 @@
-import React from 'react'
+import React from "react";
 
 const signupValidation = (formData) => {
-    const errors = {};
+  const errors = {};
 
-    //Trimming
-    const firstName = formData.firstName.trim();
-    const lastName = formData.lastName.trim();
-    const phoneNo = formData.phoneNo.trim();
-    const address = formData.address.trim();
-    const email = formData.email.trim();
-    const password = formData.password.trim();
-    const confirmPassword = formData.confirmPassword.trim();
+  //Trimming
+  const firstName = formData.firstName.trim();
+  const lastName = formData.lastName.trim();
+  const phoneNo = formData.phoneNo.trim();
+  const address = formData.address.trim();
+  const email = formData.email.trim();
+  const password = formData.password.trim();
+  const confirmPassword = formData.confirmPassword.trim();
 
-    //check emppty
-    if (!firstName) errors.firstName = 'First name is required';
-    if (!lastName) errors.lastName = 'Last name is required';
-    if (!address) errors.address = 'Address is required';
+  //check emppty
+  if (!firstName) errors.firstName = "First name is required";
+  if (!lastName) errors.lastName = "Last name is required";
+  if (!address) errors.address = "Address is required";
 
-    //phoneNo validation
-    if (!phoneNo) {
-        errors.phoneNo = 'Phone number is required';
-    } else if (!/^(?:\+94|0)[1-9][0-9]{8}$/.test(phoneNo)) {
-        errors.phoneNo = 'Invalid phone number'
-    }
+  //phoneNo validation
+  if (!phoneNo) {
+    errors.phoneNo = "Phone number is required";
+  } else if (!/^(?:\+94|0)[1-9][0-9]{8}$/.test(phoneNo)) {
+    errors.phoneNo = "Invalid phone number";
+  }
 
-    //email validation
-    if (!email) {
-        errors.email = 'Email is required';
-    }
-    else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
-        errors.email = "Invalid email format";
-    }
+  //email validation
+  if (!email) {
+    errors.email = "Email is required";
+  } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    errors.email = "Invalid email format";
+  }
 
-    //password validation
-    const passwordValidation = (password) => {
-        if (!password) return 'Password is required';
-        if (password.length < 8) return 'Password must be at least 8 characters';
-        if (!/[a-z]/.test(password)) return 'Password must include at least one lowercase letter';
-        if (!/[A-Z]/.test(password)) return 'Password must include at least one uppercase letter';
-        if (!/\d/.test(password)) return 'Password must include at least one digit';
-        if (!/[\W_]/.test(password)) return 'Password must include at least one special character';
-        if (/\s/.test(password)) return 'Password must not contain spaces';
-    };
-    const passwordError = passwordValidation(password);
-    if (passwordError) errors.password = passwordError;
+  //password validation
+  const passwordValidation = (password) => {
+    if (!password) return "Password is required";
+    if (password.length < 8) return "Password must be at least 8 characters";
+    if (!/[a-z]/.test(password))
+      return "Password must include at least one lowercase letter";
+    if (!/[A-Z]/.test(password))
+      return "Password must include at least one uppercase letter";
+    if (!/\d/.test(password)) return "Password must include at least one digit";
+    if (!/[\W]/.test(password))
+      return "Password must include at least one special character";
+  };
+  const passwordError = passwordValidation(password);
+  if (passwordError) errors.password = passwordError;
 
-    //confirm password
-    if (!confirmPassword) {
-        errors.confirmPassword = 'Confirm password is required';
-    } else if (password && password !== confirmPassword) {
-        errors.confirmPassword = 'Password does not match';
-    }
+  //confirm password
+  if (!confirmPassword) {
+    errors.confirmPassword = "Confirm password is required";
+  } else if (password && password !== confirmPassword) {
+    errors.confirmPassword = "Password does not match";
+  }
 
-    return errors;
-}
+  return errors;
+};
 
-export default signupValidation
+export default signupValidation;
