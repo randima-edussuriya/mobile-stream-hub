6db5f6c3824fd6f7d33a8be40fbe9ed87620831f
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-11-26 10:47:29 +0530

    feat: add verify-otp API route with validation middleware

--- backend/routes/shared/auth.routes.js
+++ backend/routes/shared/auth.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { sendVerifyOtp } from "../../controllers/shared/auth.controller.js";
+import { sendVerifyOtp, verifyOtp } from "../../controllers/shared/auth.controller.js";
+import { validateSendVerifyOtp, validateVerifyOtp } from "../../middleware/shared/validations.middleware.js";
 
 const router = express.Router();
 
 // base: /api/shared/auth
-router.post("/send-verify-otp", sendVerifyOtp);
+router.post("/send-verify-otp", validateSendVerifyOtp, sendVerifyOtp);
+router.post("/verify-otp", validateVerifyOtp, verifyOtp);
 
 export default router;
