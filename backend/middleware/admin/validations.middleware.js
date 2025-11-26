10ce5354a2e59b29482febc2dada0ea23d0bf243
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-11-26 14:55:35 +0530

    feat: create validateRegister middleware and link with API route

---
+++ backend/middleware/admin/validations.middleware.js
@@ -0,0 +1,77 @@
+export const validateRegister = (req, res, next) => {
+  const firstName = String(req.body.firstName || "").trim();
+  const lastName = String(req.body.lastName || "").trim();
+  const password = String(req.body.password || "").trim();
+  const email = String(req.body.email || "")
+    .trim()
+    .toLocaleLowerCase();
+  const phoneNo = String(req.body.phoneNo || "").trim();
+  const nicNo = String(req.body.nicNo || "").trim();
+  const address = String(req.body.address || "").trim();
+  const staffTypeId = String(req.body.staffTypeId || "").trim();
+  const purpose = String(req.body.purpose || "").trim();
+
+  // validate empty fields
+  if (
+    !firstName ||
+    !lastName ||
+    !password ||
+    !email ||
+    !phoneNo ||
+    !nicNo ||
+    !address ||
+    !staffTypeId ||
+    !purpose
+  ) {
+    return res
+      .status(400)
+      .json({ success: false, message: "All fields are required" });
+  }
+
+  // validate password strength
+  if (
+    !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+      password
+    )
+  ) {
+    return res.status(400).json({
+      success: false,
+      message:
+        "Password must include at least one lowercase letter, one uppercase letter, one digit, one special character and be at least 8 characters long",
+    });
+  }
+
+  // validate email format
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/.test(email)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid email format" });
+  }
+
+  // validate phone number format
+  if (!/^(?:\+94|0)[1-9][0-9]{8}$/.test(phoneNo)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid phone number format" });
+  }
+
+  // validate NIC format
+  if (!/^[0-9]{9}[vV]$|^[0-9]{12}$/.test(nicNo)) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Invalid NIC number format" });
+  }
+
+  req.body = {
+    firstName,
+    lastName,
+    password,
+    email,
+    phoneNo,
+    nicNo,
+    address,
+    staffTypeId,
+    purpose,
+  };
+  next();
+};
--- backend/routes/admin/authRoutes.js
+++ backend/routes/admin/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { login, register } from "../../controllers/admin/authController.js";
+import { validateRegister } from "../../middleware/admin/validations.middleware.js";
+import { isVerifiedOtp } from "../../middleware/shared/auth.middleware.js";
 
 const router = express.Router();
 
 // base: /api/admin/auth
-router.post("/register", register);
+router.post("/register", validateRegister, isVerifiedOtp, register);
 router.post("/login", login);
 
 export default router;
