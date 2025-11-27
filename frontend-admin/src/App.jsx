5dd7483e92ec005e37b65929ca93a6dac9261e52
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2025-11-27 22:26:28 +0530

    feat: secure routes and improve code

--- frontend-admin/src/App.jsx
+++ frontend-admin/src/App.jsx
@@ -1,204 +1,202 @@
-import React from 'react'
-import Sidebar from './components/Sidebar/Sidebar'
-import './App.css'
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
-import Home from './pages/Home'
-import { useState } from 'react'
-import Navbar from './components/Navbar'
-import Login from './pages/Login'
-import StaffManagement from './pages/StaffManagement/StaffManagement'
-import { createContext } from 'react'
-import { PrivateRoute, RoleRoute } from './routes/authRoutes'
-import CustomerManagement from './pages/CustomerManagement'
-import ItemManagement from './pages/ItemManagement'
-import OrderManagement from './pages/OrderManagement'
-import DeliveryManagement from './pages/DeliveryManagement'
-import ReorderManagement from './pages/ReorderManagement'
-import DayOffManagement from './pages/DayOffManagement'
-import LoyaltyProgramManagement from './pages/LoyaltyProgramManagement'
-import FeedbackRatingManagement from './pages/FeedbackRatingManagement'
-import CustomerSupportManagement from './pages/CustomerSupportManagement'
-import ReportsManagement from './pages/ReportsManagement'
-import StaffRegister from './pages/StaffManagement/StaffRegister'
-import RepairManagement from './pages/RepairManagement'
-import CategoryAdd from './pages/CategoryManagement/CategoryAdd'
-import CategoryManagement from './pages/CategoryManagement/CategoryManagement'
+import Sidebar from "./components/Sidebar/Sidebar";
+import "./App.css";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import { useContext, useState } from "react";
+import Navbar from "./components/Navbar";
+import StaffManagement from "./pages/StaffManagement/StaffManagement";
+import { PrivateRoute, RoleRoute } from "./routes/authRoutes";
+import Login from "./pages/Login";
+import CustomerManagement from "./pages/CustomerManagement";
+import ItemManagement from "./pages/ItemManagement";
+import OrderManagement from "./pages/OrderManagement";
+import DeliveryManagement from "./pages/DeliveryManagement";
+import ReorderManagement from "./pages/ReorderManagement";
+import DayOffManagement from "./pages/DayOffManagement";
+import LoyaltyProgramManagement from "./pages/LoyaltyProgramManagement";
+import FeedbackRatingManagement from "./pages/FeedbackRatingManagement";
+import CustomerSupportManagement from "./pages/CustomerSupportManagement";
+import ReportsManagement from "./pages/ReportsManagement";
+import StaffRegister from "./pages/StaffManagement/StaffRegister";
+import RepairManagement from "./pages/RepairManagement";
+import CategoryAdd from "./pages/CategoryManagement/CategoryAdd";
+import CategoryManagement from "./pages/CategoryManagement/CategoryManagement";
+import { AppContext } from "./context/AppContext";
 
 function App() {
   const [toggle, setToggle] = useState(false);
-  const Toggle = () => {
-    setToggle(pre => !pre)
-  }
 
-  const { currentUser } = createContext(AudioContext);
+  const { authChecked, isLoggedIn, userData } = useContext(AppContext);
 
   const Layout = () => {
     return (
-      <div className='container-fluid bg-dark min-vh-100'>
-        <div className='row'>
-          {!toggle && <div className='col-4 col-md-3 col-lg-2 bg-primary-subtle'>
-            <Sidebar />
-          </div>}
-          <div className={toggle ? 'col' : 'col-8 col-md-9 col-lg-10'}>
-            <Navbar Toggle={Toggle} />
+      <div className="container-fluid bg-dark min-vh-100">
+        <div className="row">
+          {!toggle && (
+            <div className="col-4 col-md-3 col-lg-2 bg-primary-subtle">
+              <Sidebar />
+            </div>
+          )}
+          <div className={toggle ? "col" : "col-8 col-md-9 col-lg-10"}>
+            <Navbar
+              onToggle={() => {
+                setToggle((pre) => !pre);
+              }}
+            />
             <Outlet />
           </div>
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: "/",
       element: <Login />,
-      errorElement: <div className='text-danger'>Not Found: Invalid URL</div>,
+      errorElement: <div className="text-danger">Not Found: Invalid URL</div>,
     },
     {
-      path: '/',
+      path: "/",
       element: (
-        <PrivateRoute>
+        <PrivateRoute authChecked={authChecked} isLoggedIn={isLoggedIn}>
           <Layout />
         </PrivateRoute>
       ),
       children: [
         {
-          path: 'home',
-          element: <Home />
+          path: "home",
+          element: <Home />,
         },
         {
-          path: 'staff-management',
+          path: "staff-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <StaffManagement />
             </RoleRoute>
-          )
-
+          ),
         },
         {
-          path: 'staff-register',
+          path: "staff-register",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <StaffRegister />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'customer-management',
+          path: "customer-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <CustomerManagement />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'category-management',
+          path: "category-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <CategoryManagement />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'category-add',
+          path: "category-add",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <CategoryAdd />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'item-management',
+          path: "item-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <ItemManagement />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'order-management',
+          path: "order-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <OrderManagement />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'delivery-management',
+          path: "delivery-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <DeliveryManagement />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'repair-management',
+          path: "repair-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <RepairManagement />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'reorder-management',
+          path: "reorder-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <ReorderManagement />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'day-off-management',
+          path: "day-off-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <DayOffManagement />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'loyalty-program-management',
+          path: "loyalty-program-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <LoyaltyProgramManagement />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'feedback-rating-management',
+          path: "feedback-rating-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <FeedbackRatingManagement />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'customer-support-management',
+          path: "customer-support-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <CustomerSupportManagement />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'reports-management',
+          path: "reports-management",
           element: (
-            <RoleRoute allowedRoles={['admin']}>
+            <RoleRoute userData={userData} allowedRoles={["admin"]}>
               <ReportsManagement />
             </RoleRoute>
-          )
+          ),
         },
         {
-          path: 'unauthorized',
-          element: <div className='text-bg-danger ps-1'>Access Denied !</div>
-        }
-      ]
+          path: "unauthorized",
+          element: <div className="text-bg-danger ps-1">Access Denied !</div>,
+        },
+      ],
     },
+  ]);
 
-  ])
-
-  return (
-    <RouterProvider router={router} />
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
