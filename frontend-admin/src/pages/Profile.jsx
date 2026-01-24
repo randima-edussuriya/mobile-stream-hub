632e90b55f003238143f12362b32d32efb231f43
randima-edussuriya <152683475+randima-edussuriya@users.noreply.github.com>
2026-01-24 21:08:46 +0530

    fix: add margin

--- frontend-admin/src/pages/Profile.jsx
+++ frontend-admin/src/pages/Profile.jsx
@@ -30,13 +30,13 @@ function Profile() {
       setStaffUser({});
       setError("");
       const { data } = await axios.get(
-        `${backendUrl}/api/admin/staff-users/me`
+        `${backendUrl}/api/admin/staff-users/me`,
       );
       setStaffUser(data.data);
     } catch (error) {
       setError(
         error?.response?.data?.message ||
-          "Something went wrong. Please try again."
+          "Something went wrong. Please try again.",
       );
       console.error(error);
     } finally {
@@ -82,105 +82,107 @@ function Profile() {
   }
 
   return (
-    <Container className="bg-secondary-subtle rounded shadow p-3 mt-3">
-      <Row>
-        <Col>
-          <h4>
-            {staffUser.first_name} {staffUser.last_name}
-          </h4>
-          <Form.Check
-            type="switch"
-            checked={staffUser.is_active}
-            label={
-              <span
-                className={`fw-semibold ${
-                  staffUser.is_active ? "text-success" : "text-danger"
-                }`}
-              >
-                {staffUser.is_active ? "Active" : "Inactive"}
-              </span>
-            }
-          />
-        </Col>
-        <Col xs="auto">
-          <Button
-            className="border-2 shadow"
-            variant="secondary"
-            onClick={() => navigate(-1)}
-          >
-            Back
-          </Button>
-        </Col>
-      </Row>
+    <Container>
+      <Container className="bg-secondary-subtle rounded shadow p-3 mt-3">
+        <Row>
+          <Col>
+            <h4>
+              {staffUser.first_name} {staffUser.last_name}
+            </h4>
+            <Form.Check
+              type="switch"
+              checked={staffUser.is_active}
+              label={
+                <span
+                  className={`fw-semibold ${
+                    staffUser.is_active ? "text-success" : "text-danger"
+                  }`}
+                >
+                  {staffUser.is_active ? "Active" : "Inactive"}
+                </span>
+              }
+            />
+          </Col>
+          <Col xs="auto">
+            <Button
+              className="border-2 shadow"
+              variant="secondary"
+              onClick={() => navigate(-1)}
+            >
+              Back
+            </Button>
+          </Col>
+        </Row>
 
-      {/* ------------------------------------------------
+        {/* ------------------------------------------------
             Contact details section
       ---------------------------------------------------- */}
-      <Row>
-        <Col md={6} className="mt-3">
-          <h6>Contact</h6>
-          <hr className="mt-0 border-1" />
-          <Row>
-            <Col>
-              <Form.Group className="mb-2">
-                <Form.Label>First name</Form.Label>
-                <Form.Control value={staffUser.first_name} disabled />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="mb-2">
-                <Form.Label>Last name</Form.Label>
-                <Form.Control value={staffUser.last_name} disabled />
-              </Form.Group>
-            </Col>
-          </Row>
-          <Form.Group className="mb-2">
-            <Form.Label>Email</Form.Label>
-            <Form.Control value={staffUser.email} disabled />
-          </Form.Group>
-          <Form.Group className="mb-2">
-            <Form.Label>Phone number</Form.Label>
-            <Form.Control value={staffUser.phone_number} disabled />
-          </Form.Group>
-          <Form.Group className="mb-2">
-            <Form.Label>Address</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={2}
-              value={staffUser.address}
-              disabled
-            />
-          </Form.Group>
-        </Col>
+        <Row>
+          <Col md={6} className="mt-3">
+            <h6>Contact</h6>
+            <hr className="mt-0 border-1" />
+            <Row>
+              <Col>
+                <Form.Group className="mb-2">
+                  <Form.Label>First name</Form.Label>
+                  <Form.Control value={staffUser.first_name} disabled />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group className="mb-2">
+                  <Form.Label>Last name</Form.Label>
+                  <Form.Control value={staffUser.last_name} disabled />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Form.Group className="mb-2">
+              <Form.Label>Email</Form.Label>
+              <Form.Control value={staffUser.email} disabled />
+            </Form.Group>
+            <Form.Group className="mb-2">
+              <Form.Label>Phone number</Form.Label>
+              <Form.Control value={staffUser.phone_number} disabled />
+            </Form.Group>
+            <Form.Group className="mb-2">
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={2}
+                value={staffUser.address}
+                disabled
+              />
+            </Form.Group>
+          </Col>
 
-        {/* -------------------------------------------------
+          {/* -------------------------------------------------
               Employment details section
         ------------------------------------------------------ */}
-        <Col md={6} className="mt-3">
-          <h6>Employment</h6>
-          <hr className="mt-0" />
-          <Form.Group className="mb-2">
-            <Form.Label>User ID</Form.Label>
-            <Form.Control value={staffUser.staff_id} disabled />
-          </Form.Group>
-          <Form.Group className="mb-2">
-            <Form.Label>Role</Form.Label>
-            <Form.Control value={staffUser.user_role} disabled />
-          </Form.Group>
-          <Form.Group className="mb-2">
-            <Form.Label>Hire date</Form.Label>
-            <Form.Control
-              value={dayjs(staffUser.hire_date).format("YYYY-MM-DD HH:mm:ss")}
-              disabled
-            />
-          </Form.Group>
-          <Form.Group className="mb-2">
-            <Form.Label>NIC</Form.Label>
-            <Form.Control value={staffUser.nic_number} disabled />
-          </Form.Group>
-          <Form.Group className="mb-2"></Form.Group>
-        </Col>
-      </Row>
+          <Col md={6} className="mt-3">
+            <h6>Employment</h6>
+            <hr className="mt-0" />
+            <Form.Group className="mb-2">
+              <Form.Label>User ID</Form.Label>
+              <Form.Control value={staffUser.staff_id} disabled />
+            </Form.Group>
+            <Form.Group className="mb-2">
+              <Form.Label>Role</Form.Label>
+              <Form.Control value={staffUser.user_role} disabled />
+            </Form.Group>
+            <Form.Group className="mb-2">
+              <Form.Label>Hire date</Form.Label>
+              <Form.Control
+                value={dayjs(staffUser.hire_date).format("YYYY-MM-DD HH:mm:ss")}
+                disabled
+              />
+            </Form.Group>
+            <Form.Group className="mb-2">
+              <Form.Label>NIC</Form.Label>
+              <Form.Control value={staffUser.nic_number} disabled />
+            </Form.Group>
+            <Form.Group className="mb-2"></Form.Group>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 }
